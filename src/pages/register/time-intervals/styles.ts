f5f12e32fb897b4d64fd43cac3643fb3d9b8636c
JVMPR.DEV <jvmpr.dev@gmail.com>
2023-03-28 12:10:30 -0300

    Feature - zod input and output

--- src/pages/register/time-intervals/styles.ts
+++ src/pages/register/time-intervals/styles.ts
@@ -1,4 +1,4 @@
-import { Box, styled } from '@ignite-ui/react'
+import { Box, styled, Text } from '@ignite-ui/react'
 
 export const IntervalBox = styled(Box, {
   marginTop: '$6',
@@ -17,7 +17,6 @@ export const IntervalItem = styled('div', {
   alignItems: 'center',
   justifyContent: 'space-between',
   padding: '$3 $4',
-
   '& + &': {
     borderTop: '1px solid $gray600',
   },
@@ -33,8 +32,12 @@ export const IntervalInputs = styled('div', {
   display: 'flex',
   alignItems: 'center',
   gap: '$2',
-
   'input::-webkit-calendar-picker-indicator': {
     filter: 'invert(100%) brightness(40%)',
   },
+})
+
+export const FormError = styled(Text, {
+  color: '#f75a68',
+  marginBottom: '$4',
 })

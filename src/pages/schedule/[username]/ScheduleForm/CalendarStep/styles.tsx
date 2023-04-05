9fb62302f44b0e8843eec6071e0e403c35039a5d
JVMPRDEV <jvmpr.dev@gmail.com>
2023-04-05 12:13:53 -0300

    Feature - preparing calendar

--- src/pages/schedule/[username]/ScheduleForm/CalendarStep/styles.tsx
+++ src/pages/schedule/[username]/ScheduleForm/CalendarStep/styles.tsx
@@ -1,22 +1,84 @@
-import { Heading, styled, Text } from '@ignite-ui/react'
+import { Box, styled, Text } from '@ignite-ui/react'
 
-export const Container = styled('div', {
-  maxWidth: 852,
-  padding: '0 $4',
-  margin: '$20 auto $4',
+export const Container = styled(Box, {
+  margin: '$6 auto 0',
+  padding: 0,
+  display: 'grid',
+  position: 'relative',
+
+  variants: {
+    isTimePickerOpen: {
+      true: {
+        gridTemplateColumns: '1fr 280px',
+
+        '@media(max-width: 900px)': {
+          gridTemplateColumns: '1fr',
+        },
+      },
+      false: {
+        width: 540,
+        gridTemplateColumns: '1fr',
+      },
+    },
+  },
 })
 
-export const UserHeader = styled('div', {
-  display: 'flex',
-  flexDirection: 'column',
-  alignItems: 'center',
+export const TimePicker = styled('div', {
+  borderLeft: '1px solid $gray600',
+  padding: '$6 $6 0',
+  overflowY: 'scroll',
 
-  [`> ${Heading}`]: {
-    lineHeight: '$base',
-    marginTop: '$2',
+  position: 'absolute',
+  top: 0,
+  bottom: 0,
+  right: 0,
+  width: 280,
+})
+
+export const TimePickerHeader = styled(Text, {
+  fontWeight: '$medium',
+
+  span: {
+    color: '$gray200',
+  },
+})
+
+export const TimePickerList = styled('div', {
+  marginTop: '$3',
+  display: 'grid',
+  gridTemplateColumns: '1fr',
+  gap: '$2',
+
+  '@media (max-width: 900px)': {
+    gridTemplateColumns: '2fr',
+  },
+})
+
+export const TimePickerItem = styled('button', {
+  border: 0,
+  backgroundColor: '$gray600',
+  padding: '$2 0',
+  cursor: 'pointer',
+  color: '$gray100',
+  borderRadius: '$sm',
+  fontSize: '$sm',
+  lineHeight: '$base',
+
+  '&:last-child': {
+    marginBottom: '$6',
   },
 
-  [`> ${Text}`]: {
-    color: '$gray200',
+  '&:disabled': {
+    background: 'none',
+    cursor: 'default',
+    opacity: 0.4,
+  },
+
+  '&:not(:disabled):hover': {
+    background: '$gray500',
+  },
+
+  '&:focus': {
+    boxShadow: '0 0 0 2px $colors$gray100',
   },
 })
